8f6f602df44ed1868f57d6a1282efac90d38a828
Jeremy Vanpé <jeremy.vanpe@ehealth.fgov.be>
2018-02-10 22:08:54 +0100

    Add exercise 6.1

--- src/main/java/be/jeremy/functional/programming/exercise/optional/Option.java
+++ src/main/java/be/jeremy/functional/programming/exercise/optional/Option.java
@@ -6,19 +6,23 @@ package be.jeremy.functional.programming.exercise.optional;
 public abstract class Option<T> {
 
     public abstract T getOrThrow();
+    public abstract T getOrElse(T defaultValue);
 
     @SuppressWarnings("rawtypes")
     private static Option none = new None();
 
     private static class None<T> extends Option<T> {
 
-        private None() {
-
-        }
+        private None() {}
 
         @Override
         public T getOrThrow() {
             throw new IllegalArgumentException("get called on None");
+        }
+
+        @Override
+        public T getOrElse(T defaultValue) {
+            return defaultValue;
         }
 
         @Override
@@ -37,6 +41,11 @@ public abstract class Option<T> {
 
         @Override
         public T getOrThrow() {
+            return value;
+        }
+
+        @Override
+        public T getOrElse(T defaultValue) {
             return value;
         }
 
