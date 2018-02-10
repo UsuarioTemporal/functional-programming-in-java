f4abb1430c22d441c0f094e242911b6cac396be3
Jeremy Vanpé <jeremy.vanpe@ehealth.fgov.be>
2018-02-10 22:15:16 +0100

    Add exercise 6.2

--- src/main/java/be/jeremy/functional/programming/exercise/optional/Option.java
+++ src/main/java/be/jeremy/functional/programming/exercise/optional/Option.java
@@ -1,5 +1,7 @@
 package be.jeremy.functional.programming.exercise.optional;
 
+import java.util.function.Supplier;
+
 /**
  * @author Jeremy
  */
@@ -7,6 +9,7 @@ public abstract class Option<T> {
 
     public abstract T getOrThrow();
     public abstract T getOrElse(T defaultValue);
+    public abstract T getOrElse(Supplier<T> defaultValueSupplier);
 
     @SuppressWarnings("rawtypes")
     private static Option none = new None();
@@ -23,6 +26,11 @@ public abstract class Option<T> {
         @Override
         public T getOrElse(T defaultValue) {
             return defaultValue;
+        }
+
+        @Override
+        public T getOrElse(Supplier<T> defaultValueSupplier) {
+            return defaultValueSupplier.get();
         }
 
         @Override
@@ -46,6 +54,11 @@ public abstract class Option<T> {
 
         @Override
         public T getOrElse(T defaultValue) {
+            return value;
+        }
+
+        @Override
+        public T getOrElse(Supplier<T> defaultValueSupplier) {
             return value;
         }
 
